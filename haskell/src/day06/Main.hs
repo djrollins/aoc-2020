1dbb8386a88be299d5e17ab316ee6ef1b3a26de6
Daniel J. Rollins <daniel@djrollins.com>
2020-12-08 22:58:12 +0000

    day 6

---
+++ haskell/src/day06/Main.hs
@@ -0,0 +1,24 @@
+module Main (main) where
+
+import System.Directory (getCurrentDirectory)
+import System.Environment (getArgs)
+import System.FilePath ((</>))
+import System.IO ()
+import Data.List.Split
+import Data.List
+
+part1 :: String -> Int
+part1 = sum . map (length . nub . filter (/= '\n')) . splitOn "\n\n"
+
+part2 :: String -> Int
+part2 = sum . map (length . nub . foldr intersect ['a'..'z'] . lines) . splitOn "\n\n"
+
+main :: IO ()
+main = do
+  currentDirectory <- getCurrentDirectory
+  filename <- head <$> getArgs
+  let filePath = currentDirectory </> filename
+  content <- readFile filePath
+
+  print $ part1 content
+  print $ part2 content
